3f5d10da3a4bcfd84dc2e734cbd48d975644461f
guiguilins <guiguilins56@gmail.com>
2024-08-14 15:21:28 -0300

    User insert, delete and update

--- spring-vscode/src/main/java/com/teste/spring_vscode/resources/UserResource.java
+++ spring-vscode/src/main/java/com/teste/spring_vscode/resources/UserResource.java
@@ -12,10 +12,13 @@ import com.teste.spring_vscode.services.UserService;
 import java.net.URI;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 
 @RestController
@@ -44,4 +47,15 @@ public class UserResource {
         return ResponseEntity.created(uri).body(obj);
     }
     
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj) {
+        obj = service.update(id, obj);
+        return ResponseEntity.ok().body(obj);
+    }
 }
