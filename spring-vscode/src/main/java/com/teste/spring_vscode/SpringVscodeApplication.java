4ecab62e110f1a243560b535790013bdf33684bb
guiguilins <guiguilins56@gmail.com>
2024-08-13 22:55:33 -0300

    first commit

---
+++ spring-vscode/src/main/java/com/teste/spring_vscode/SpringVscodeApplication.java
@@ -0,0 +1,13 @@
+package com.teste.spring_vscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVscodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringVscodeApplication.class, args);
+	}
+
+}
---
+++ spring-vscode/src/main/java/com/teste/spring_vscode/resources/UserResource.java
@@ -0,0 +1,22 @@
+package com.teste.spring_vscode.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.teste.spring_vscode.entities.User;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping(value = "/")
+    public ResponseEntity<User> findAll() {
+        User u = new User(1L, "Maria", "maria@gmail.com", "9999999", "1234565");
+        return ResponseEntity.ok().body(u);
+
+    }
+}
